19634bb50285399cfbfd0521620841460f89a26b
Zanele <zanele@thedigitalacademy.co.za>
2020-08-13 14:38:14 +0200

    service created

---
+++ Frontend/src/app/components/shopping-cart/shopping-cart-response.ts
@@ -0,0 +1,27 @@
+export class ShoppingCartResponse {
+    name:string;
+surname:string;
+gender:string;
+phone:string;
+email:string;
+city:string;
+province:string;
+township:string;
+street:string;
+code:string;
+product:[];
+
+    constructor(){
+        this.name = "wertfygu";
+        this.surname= "fgjkl";
+        this.gender = "frjk";
+        this.phone = "rtjkl";
+        this.email = "ertf;";
+        this.city = "seu";
+        this.province = "qwj";
+        this.township = "dgfhjk";
+        this.street = "sfjk";
+        this.code = "esdrhjk";
+        this.product = []
+    }
+}
--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  URL :" API_URL: 'http://localhost:5000'"
 };
